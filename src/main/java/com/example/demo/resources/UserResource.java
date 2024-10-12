96da0201dd280d65876581be9708e85762c81664
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-10-11 21:22:31 -0300

    connecting to MongoDB with repository and service | updating application.properties | dependency groupId >org.springframework.boot< and artifactId >spring-boot-starter-data-mongodb

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,27 +1,26 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService; 
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Silva", "maria@gmail.com");
-		User alex = new User("2", "Alex Silva", "alex@gmail.com");
-		User jo = new User("3", "João Silva", "joão@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex, jo));
+		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
